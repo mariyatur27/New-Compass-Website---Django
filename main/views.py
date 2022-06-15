8751c40a2ff9d983af370096e16f90c4e6f5fbce
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2022-06-14 20:05:42 -0400

    added html files
    added html files to the main app
    - base.html
    - homePage.html
    - aboutUs.html

--- main/views.py
+++ main/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homePage(response):
-    return HttpResponse('home page')
+    return render(response, "main/homePage.html", {})
 
 
 def aboutUs(response):
-    return HttpResponse('about us')
+    return render(response, "main/aboutUs.html", {})
