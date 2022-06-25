6b0f8a309b01865e57ec37deb072a7458805ed9a
Ali Rahbar <72822222+crypto-a@users.noreply.github.com>
2022-06-24 23:12:02 -0400

    fixed the admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
-admin.site.register(Account)
 
+class AccountAdmin(UserAdmin):
+    list_display = ('username', 'email',)
+    search_fields = ('username', 'email',)
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
